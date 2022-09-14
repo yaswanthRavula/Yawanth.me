bb88e685c077430c846b636cc4ccc3131586fb67
Yaswanth <yaswanthravula0@gmail.com>
2022-09-14 18:06:39 +0530

    dmwsd

--- client/server.js
+++ client/server.js
@@ -23,7 +23,7 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-app.all('/*', function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
