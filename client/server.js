de29d3052da72eb5c9942d76aa203b9177d1b7f3
Yaswanth <yaswanthravula0@gmail.com>
2022-09-25 16:48:57 +0530

    d;,dffdf

--- client/server.js
+++ client/server.js
@@ -17,13 +17,13 @@ const reviewController=require("./review-controller");
 
 app.listen((process.env.PORT || 9000),()=>{console.log("Port is listeing at 9000")});
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '/*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-app.all('/*', function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
