aa254fc6c245e32cadc748d51b40d42f08a35172
Yaswanth <yaswanthravula0@gmail.com>
2022-10-26 17:36:16 +0530

    dnbs d sldn s

--- backend yaswanth.me/server.js
+++ backend yaswanth.me/server.js
@@ -7,22 +7,17 @@ const cors=require('cors');
 const http=require('http');
 app.use(cors());
 
-app.get('/*', (req,res)=>{
-    res.sendFile(path.join(__dirname))
-})
 
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
   });
-  const server=http.createServer(app);
-  
 const messageController=require("./message-controller");
 const reviewController=require("./review-controller");
 const PORT=process.env.PORT||9000;
 
-server.listen((PORT),()=>{console.log("Port is listeing at ",process.env.PORT)});
+app.listen((PORT),()=>{console.log("Port is listeing at ",process.env.PORT)});
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
